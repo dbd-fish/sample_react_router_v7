ae941a276ad6fe94a4b305d4bc9e42f7aa38452d
dbd-fish <ryo.kokai.g@gmail.com>
2024-12-31 15:55:11 +0900

    ホーム画面も共通ヘッダーを使うように修正

--- sample_frontend_container/sample_remix_react/app/routes/home.tsx
+++ sample_frontend_container/sample_remix_react/app/routes/home.tsx
@@ -1,7 +1,7 @@
-import Header from '../components/header/LoggedOutHeader';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function MyPage() {
+export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
