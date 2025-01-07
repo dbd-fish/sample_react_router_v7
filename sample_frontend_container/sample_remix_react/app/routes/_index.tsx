1f037d40b263a0f6a1d71834d17310fa14167257
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-07 21:55:33 +0900

    コード整理

--- sample_frontend_container/sample_remix_react/app/routes/_index.tsx
+++ sample_frontend_container/sample_remix_react/app/routes/_index.tsx
@@ -3,8 +3,7 @@ import {
   type LoaderFunction,
   type MetaFunction,
 } from '@remix-run/node';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+
 
 export const loader: LoaderFunction = async () => {
   return redirect('/home'); // 'home.tsx' にリダイレクト
