bc43a8526924afaf966e873f46a1d5429e3b660d
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-12 20:52:48 +0900

    テスト用にidを追加。いくつかテストも作成

--- cypress_container/cypress.config.js
+++ cypress_container/cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://frontend:5173', // テスト対象のURL
-    specPattern: 'e2e/cypress/**/*.cy.js', // 修正: 正しいパスを指定
+    specPattern: [
+       'e2e/cypress/**/*.cy.js', //ローカル環境におけるE2Eテスト
+      'front_st/**/*.cy.js',  //画面単位のテスト
+    ], 
     supportFile: 'cypress/support/e2e.js', //     // ブラウザ設定を追加
     // browser: {
     //   chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox']
