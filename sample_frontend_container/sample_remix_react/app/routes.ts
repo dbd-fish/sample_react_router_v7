19419ed4f841a696d0b5cefcb0c2e83ad798591b
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-16 13:52:38 +0900

    同一のtsxファイルを異なるルーティングに指定できないため、home.tsxを一つのルーティングで使用するように修正

--- sample_frontend_container/sample_remix_react/app/routes.ts
+++ sample_frontend_container/sample_remix_react/app/routes.ts
@@ -2,12 +2,8 @@ import { type RouteConfig, route, index } from '@react-router/dev/routes';
 
 export default [
   // ルートパスに対応するコンポーネント
-  // index('./routes/index.tsx'),
-
+  index('./routes/home.tsx'), // NOTE: 同一のtsxファイルを異なるルーティングに指定できないため、home.tsxはここのみで使用
   // その他のルート
-  // route('', './routes/home.tsx'),
-
-  route('home', './routes/home.tsx'),
   route('login', './routes/login.tsx'),
   route('mypage', './routes/mypage.tsx'),
 ] satisfies RouteConfig;
